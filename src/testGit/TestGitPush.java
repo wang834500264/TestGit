a9ceb66e357ab3b7fbe2b6067fb42a3662dbe2c6
wang834500264 <834500264@qq.com>
2019-04-23 20:47:21 +0800

    merge done

--- src/testGit/TestGitPush.java
+++ src/testGit/TestGitPush.java
@@ -3,15 +3,7 @@ package testGit;
 public class TestGitPush {
 	public static void main(String[] args) {
 		System.out.println("我是你爸爸!");
-<<<<<<< HEAD
 		System.out.println("11111111");
-		System.out.println("22222222");
-=======
-		System.out.println("11111111");
-		System.out.println("11111111");
->>>>>>> branch 'master' of https://github.com/wang834500264/TestGit.git
-		System.out.println("133333333");
-		System.out.println("4444444444");
 
 	}
 }
