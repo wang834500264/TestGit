2dccbc6d19214fc1dd40cec9fac6d38fe3820e97
wang834500264 <834500264@qq.com>
2019-04-23 20:44:27 +0800

    local commit 2

--- src/testGit/TestGitPush.java
+++ src/testGit/TestGitPush.java
@@ -4,10 +4,9 @@ public class TestGitPush {
 	public static void main(String[] args) {
 		System.out.println("我是你爸爸!");
 		System.out.println("11111111");
-<<<<<<< HEAD
 		System.out.println("22222222");
-=======
 		System.out.println("133333333");
->>>>>>> branch 'master' of https://github.com/wang834500264/TestGit.git
+		System.out.println("4444444444");
+
 	}
 }
